1c9b873e9f8376d06926fd248b31597d10380be1
Jhean_Victo <jheanbarbosa1233@gmail.com>
2024-10-26 18:52:27 -0300

    Add Icon and change color button

--- app/src/main/java/com/example/screenlogin/LoginScreen.kt
+++ app/src/main/java/com/example/screenlogin/LoginScreen.kt
@@ -7,16 +7,28 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -24,10 +36,12 @@ import androidx.compose.ui.unit.sp
 fun loginScreen(
 
 ){
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
@@ -35,24 +49,43 @@ fun loginScreen(
             modifier = Modifier.size(200.dp))
         
         Text(text = "Welcome Prisma VI", fontSize = 28.sp, fontWeight = FontWeight.Bold)
-        
-        Text(text = "Login Account")
+
         
         Spacer(modifier = Modifier.height(4.dp))
         
-        OutlinedTextField(value = "", onValueChange = {}, label = {
-            Text(text = "Email Address")
-        })
+        OutlinedTextField(value = username,
+            onValueChange = {username = it},
+            label = { Text(text = "Email Address")},
+            singleLine = true,
+            leadingIcon = {
+                IconButton(onClick = {/*TODO*/}) {
+                    Icon(imageVector = Icons.Filled.AccountCircle, contentDescription = "User Icon")
+                }
+            }
+            )
         
         Spacer(modifier = Modifier.height(4.dp))
 
-        OutlinedTextField(value = "", onValueChange = {}, label = {
-            Text(text = "Password")
-        })
+        OutlinedTextField(value = "",
+            onValueChange = {},
+            label = { Text(text = "Password")},
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            leadingIcon = {
+                IconButton(onClick = {/*TODO*/}) {
+                    Icon(imageVector = Icons.Filled.Lock, contentDescription = "Password Icon")
+                }
+            }
+            )
         
         Spacer(modifier = Modifier.height(4.dp))
     
-        Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color.Magenta)) {
+        Button(onClick = { /*TODO*/ },
+           colors = ButtonDefaults.buttonColors(
+               containerColor = Color(0xFF7AE8B8)
+           )
+           ) {
             Text(text = "Login")
             
         }
